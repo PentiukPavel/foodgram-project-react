2aac20098671f639d6a7e347793cf0c1b886fe37
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-02-26 17:11:41 +0300

    Settings

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -32,7 +32,7 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name',
+        fields = ('email', 'username', 'first_name',
                   'last_name', 'password',)
 
 
