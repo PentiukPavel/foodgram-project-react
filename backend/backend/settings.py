a20b566ba7fcc8d8fb511235ead254a719075dd2
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-02-27 21:23:12 +0300

    static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,9 +124,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
