abd73b1f7488067ab46880f2776b2af7e99bdd40
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-02-28 16:51:50 +0300

    permissions are changed

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,7 +8,7 @@ class AdminAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user and request.user.is_authenticated))
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
@@ -16,4 +16,3 @@ class AdminAuthorOrReadOnly(permissions.BasePermission):
                 and (request.user.is_superuser
                      or request.user.is_staff
                      or request.user == obj.author))
-
