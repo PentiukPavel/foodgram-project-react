a3c46a39effde18bc4a1bb3f3687622219f2fd23
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-02-28 07:12:39 +0300

    filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -8,8 +8,14 @@ class RecipeFilter(FilterSet):
 
     author = CharFilter(field_name='author__username', lookup_expr='exact')
     tags = CharFilter(field_name='tags__slug', lookup_expr='exact')
-    is_favorited = BooleanFilter(field_name='favorited')
+    is_favorited = BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = BooleanFilter(field_name='in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if user.is_anonymous:
+            return queryset
+        return queryset.filter(pk=user.id)
 
     class Meta:
         model = Recipe
