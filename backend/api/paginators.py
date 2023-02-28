8685cf6ba3134c52221e148ab554b946bb808319
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-02-28 23:48:11 +0300

    pagination

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -4,5 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class SubscriptionPaginator(PageNumberPagination):
     """Кастомный пагинатор для подписок."""
 
-    page_query_param = 'page'
     page_size_query_param = 'limit'
