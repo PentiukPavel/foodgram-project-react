65a3feb7647933b06ade6c4166948743e2803352
Пеньтюк Павел <pentukpavel@yandex.ru>
2023-03-01 20:49:30 +0300

    Shopping cart

--- backend/api/views.py
+++ backend/api/views.py
@@ -56,8 +56,6 @@ class RecipeViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
         return RecipeCreateSerializer
 
     def get_queryset(self):
-        if not self.request.query_params.get('tags'):
-            return Recipe.objects.none()
         return Recipe.objects.all().select_related('author')
 
     def perform_create(self, serializer):
